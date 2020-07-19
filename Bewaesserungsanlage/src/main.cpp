c970ddb8ee44a160f55a263cbe9615a682d7df3d
AlexanderPilot <alexander.pilot@gmx.de>
2020-07-19 20:48:51 +0200

    Anpassung Ordnerstruktur

---
+++ Bewaesserungsanlage/src/main.cpp
@@ -0,0 +1,51 @@
+//Einbinden von Bibliotheken
+#include <Arduino.h>
+
+//Definition globale Variablen
+
+
+//Definition Objekte
+
+
+//Definition Funktionen
+
+void setup() {
+  //Initialisiere GPIO Pins
+    //Bodenfeuchtigkeitssensor
+
+    //Ultraschallsensor
+
+    //Pumpe
+
+    //Batteriemonitoring
+  
+  //Starte WiFi
+
+  
+  //Initialisieren Email-Server
+
+  
+}
+
+void loop() {
+  //Definition von Variabelen
+  
+  //Messe Bodenfeuchtigkeit
+
+  //Entscheidungsbaum Bodenfeuchtigkeit iO
+    //nichts tun
+
+  //Entscheidungsbaum Bodenfeuchtigkeit niO
+    //Pumpe ansteuern
+
+  //Prüfe Wasservorrat
+
+  //Entscheidungsbaum Wasservorrat iO
+    //nichts tun
+
+  //Entscheidungsbaum Wasservorrat niO
+    //Sende Mail an hinterlegten Empfänger
+
+  //Starte DeepSleep mit zyklischem Wakeup alle x Minuten
+
+}
